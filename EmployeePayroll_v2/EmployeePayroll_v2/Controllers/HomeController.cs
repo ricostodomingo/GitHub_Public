9368b155fa04bffb2bcafb9d795c98a9ab0dc58a
ricostodomingo <rico_sto_domingo@yahoo.com>
2023-12-31 11:40:16 +0800

    Initial release

---
+++ EmployeePayroll_v2/EmployeePayroll_v2/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using EmployeePayroll_v2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll_v2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public string Test()
+        {
+            Employee employee = new Employee();
+            return "MonthlyRate display name = " + employee.GetDisplayName("MonthlyRate");
+        }
+
+        public IActionResult ReadMe()
+        { 
+            return View(); 
+        }  
+    }
+}
---
+++ EmployeePayroll_v2/EmployeePayroll_v2/Models/PaySlip.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace EmployeePayroll_v2.Models
+{
+    public class PaySlip : Employee
+    {
+        [Range(0, 31, ErrorMessage = "Days present must be a number from 0 to 31.")]
+        [DisplayName("Days Present")]
+        public decimal PresentDays { get; set; }
+
+        [Range(0, 31, ErrorMessage = "Days absent must be a number from 0 to 31.")]
+        [DisplayName("Days Absent")]
+        public decimal AbsentDays { get; set; }
+
+        [RegularExpression(@"\d+(\.\d{1,2})?", ErrorMessage = "Deductions must be a positive number.")]
+        [DataType(DataType.Currency)]
+        [DisplayName("Deductions")]
+        public decimal Deductions { get; set; }
+
+        [RegularExpression(@"\d+(\.\d{1,2})?", ErrorMessage = "Taxable amount must be a positive number.")]
+        [DataType(DataType.Currency)]
+        [DisplayName("Taxable Amount")]
+        public decimal TaxableAmount { get; set; }
+
+        [RegularExpression(@"\d+(\.\d{1,2})?", ErrorMessage = "Tax must be a positive number.")]
+        [DataType(DataType.Currency)]
+        [DisplayName("Tax")]
+        public decimal Tax { get; set; }
+
+        [RegularExpression(@"\d+(\.\d{1,2})?", ErrorMessage = "Salary must be a positive number.")]
+        [DataType(DataType.Currency)]
+        [DisplayName("Salary")]
+        public decimal Salary { get; set; }
+    }
+}
